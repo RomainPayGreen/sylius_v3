95b65c7a79dbf49add937b38efac2ae510d57afe
RomainPayGreen <RomainPayGreen@users.noreply.github.com>
2026-05-29 21:20:27 +0200

    Add Sylius 2 admin gateway rendering

--- src/DependencyInjection/PayGreenSyliusPayumPluginExtension.php
+++ src/DependencyInjection/PayGreenSyliusPayumPluginExtension.php
@@ -7,10 +7,41 @@ namespace PayGreen\SyliusPayumPlugin\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-final class PayGreenSyliusPayumPluginExtension extends Extension
+final class PayGreenSyliusPayumPluginExtension extends Extension implements PrependExtensionInterface
 {
+    public function prepend(ContainerBuilder $container): void
+    {
+        if (!$container->hasExtension('sylius_twig_hooks')) {
+            return;
+        }
+
+        $container->prependExtensionConfig('sylius_twig_hooks', [
+            'hooks' => [
+                'gateway_configuration.paygreen' => [
+                    'config' => [
+                        'template' => '@PayGreenSyliusPayumPlugin/admin/payment_method/form/sections/gateway_configuration/config.html.twig',
+                        'priority' => 0,
+                    ],
+                ],
+                'sylius_admin.payment_method.create.content.form.sections.gateway_configuration.paygreen' => [
+                    'config' => [
+                        'template' => '@PayGreenSyliusPayumPlugin/admin/payment_method/form/sections/gateway_configuration/config.html.twig',
+                        'priority' => 0,
+                    ],
+                ],
+                'sylius_admin.payment_method.update.content.form.sections.gateway_configuration.paygreen' => [
+                    'config' => [
+                        'template' => '@PayGreenSyliusPayumPlugin/admin/payment_method/form/sections/gateway_configuration/config.html.twig',
+                        'priority' => 0,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
     /**
      * @param array<array-key, mixed> $configs
      */
